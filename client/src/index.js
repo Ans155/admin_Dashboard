4517a6334d32084dd7de02d705f4f2721430d8e5
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-06-28 16:34:07 +0530

    setting redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import './index.css';
 import App from './App';
+import { globalSlice } from './state/index';
+
+const rootReducer = combineReducers({
+  global: globalSlice.reducer,
+  // Add other reducers here if needed
+});
+
+const store = createStore(rootReducer); // Create the Redux store with the root reducer
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>
 );
 
---
+++ client/src/scenes/Layout/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Layout = () => {
+    //console.log("asdhasj")
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
