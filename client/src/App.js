e52c5439694301400542c3b51f3c9d07e6b44430
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-07-05 15:58:19 +0530

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/Layout/index";
 import Dashboard from "scenes/dashboard/index";
-
+import Products from "scenes/products/index"
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -19,6 +19,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
