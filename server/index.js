fc5416f5d6bc60b9328c5db36c85e133976ee138
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-07-11 00:14:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,8 +11,9 @@ import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js"
+import Transaction from "./models/Transactions.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
 
 
 
@@ -48,7 +49,7 @@ mongoose
     // OverallStat.insertMany(dataOverallStat);
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
-    // Transaction.insertMany(dataTransaction);
+    //Transaction.insertMany(dataTransaction);
     //User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
