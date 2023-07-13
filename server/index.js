9c8ecb5493e9c552e7bafbe4579a8df6998cd522
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-07-14 02:24:05 +0530

    injecting overallStat data

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ import User from "./models/User.js";
 import Product from "./models/Product.js"
 import Transaction from "./models/Transaction.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import OverallStat from "./models/OverallStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js";
 
 
 
@@ -48,7 +49,7 @@ mongoose
 
     /* ONLY ADD DATA ONE TIME */
     // AffiliateStat.insertMany(dataAffiliateStat);
-    // OverallStat.insertMany(dataOverallStat);
+    //OverallStat.insertMany(dataOverallStat);
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
